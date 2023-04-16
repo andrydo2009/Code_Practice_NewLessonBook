76203a69a3d605db061673f63cdd8dac65421f89
andrydo2009 <115743619+andrydo2009@users.noreply.github.com>
2023-04-16 22:35:37 +0300

    Читаем и консепктируем!

--- src/ch02/HelloJava.java
+++ src/ch02/HelloJava.java
@@ -2,13 +2,10 @@ package ch02;
 
 import javax.swing.*;
 
-/**
- * A bare bones graphical version of Hello World.
- */
 public class HelloJava {
     public static void main( String[] args ) {
-        JFrame frame = new JFrame( "Hello, Java!" );
-        JLabel label = new JLabel("Hello, Java!", JLabel.CENTER );
+        JFrame frame = new JFrame( "Hello, Java!" );//для размещения окна на экране содзаем обьек frame
+        JLabel label = new JLabel("Hello, Java!", JLabel.CENTER );//создаем сообщение в окне и размещаем его в центре
         frame.add(label);
         frame.setSize( 300, 300 );
         frame.setVisible( true );
--- src/ch02/HelloJava2.java
+++ src/ch02/HelloJava2.java
@@ -4,9 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-/**
- * An upgraded graphical application with interactivity!
- */
+
 public class HelloJava2 {
 	public static void main( String[] args ) {
     	JFrame frame = new JFrame( "HelloJava2" );
@@ -17,20 +15,11 @@ public class HelloJava2 {
 	}
 }
 
-/*
- * Inheritence (the "extends" keyword below) and interfaces
- * (the "implements MouseMotionListener" portion) are covered in
- * Chapter 5.
- */
+
 class HelloComponent2 extends JComponent implements MouseMotionListener {
 	String theMessage;
 	int messageX = 125, messageY = 95; // Coordinates of the message
 
-	/**
-	 * Create a new component that can draw its message at an arbitrary position.
-	 * That position can be changed by dragging the mouse; we attach a listener
-	 * to pick up those drag events.
-	 */
 	public HelloComponent2( String message ) {
     	theMessage = message;
     	addMouseMotionListener(this);
@@ -40,14 +29,14 @@ class HelloComponent2 extends JComponent implements MouseMotionListener {
     	g.drawString( theMessage, messageX, messageY );
 	}
 
-	public void mouseDragged(MouseEvent e) {
-    	// Save the mouse coordinates and paint the message.
+	public void mouseDragged(MouseEvent e) {//MouseEvent вызывает событие при движении мышью
+    	// Сохраняет координаты мыши и рисует сообщение.
     	messageX = e.getX();
     	messageY = e.getY();
     	repaint();
 	}
 
   	public void mouseMoved(MouseEvent e) { 
-		// Ignore simple movements
+		// игнориркет просые движения
 	}
 }
